53e30f95c2dd7f762005aee4c2bd0d968d91561b
mmrzaf <pherry@gmail.com>
2026-02-18 16:58:33 +0330

    added markdown support

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"OXY2DEV/markview.nvim",
+		ft = "markdown",
+		opts = {},
+		keys = {
+			{ "<leader>mp", "<cmd>Markview toggle<cr>", desc = "Markdown preview (in-buffer)" },
+		},
+	},
+}
